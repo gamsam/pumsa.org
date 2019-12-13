8a56b7bb44d0ec3d801b8a487af76fa24e979e6f
gamsam <paulshimbal@gmail.com>
2019-12-13 16:35:52 +0100

    fixed validation, admin user & post rel

---
+++ admin/dashboard.php
@@ -0,0 +1,35 @@
+<?php
+
+include_once('../path.php');
+$PageTitle = "PUMSA - Admin Section: Dashboard";
+
+function customPageHeader()
+{ ?>
+
+<?php }
+
+include(ROOT_PATH . '/app/controllers/posts.php');
+include(ROOT_PATH . '/app/includes/adminheader.php');
+
+?>
+
+
+<div class="admin-wrapper">
+
+    <?php include(ROOT_PATH . '/app/includes/adminsidebar.php'); ?>
+
+    <div class="admin-content">
+
+        <div class="content">
+            <h2 class="page-title"> Admin Dashboard </h2>
+
+            <?php include(ROOT_PATH . '/app/includes/messages.php'); ?>
+            
+        </div>
+
+    </div>
+
+</div>
+
+
+<?php include(ROOT_PATH . '/app/includes/adminfooter.php'); ?>
