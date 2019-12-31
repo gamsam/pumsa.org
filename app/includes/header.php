b3a2de7a828fd90f6f4a4d9f29bd293d8e7b9406
gamsam <paulshimbal@gmail.com>
2019-12-31 19:42:46 +0100

    tweaks

--- app/includes/header.php
+++ app/includes/header.php
@@ -49,6 +49,217 @@
   </script>
   <!--End of Tawk.to Script -->
 
+  <style>
+    /* Prelaoder */
+
+    .no-scroll-y {
+      overflow-y: hidden;
+    }
+
+    .text-center {
+      text-align: center;
+    }
+
+    .ctn-preloader {
+      -webkit-box-align: center;
+      -ms-flex-align: center;
+      align-items: center;
+      cursor: default;
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: flex;
+      height: 100%;
+      -webkit-box-pack: center;
+      -ms-flex-pack: center;
+      justify-content: center;
+      position: fixed;
+      left: 0;
+      top: 0;
+      width: 100%;
+      z-index: 9000;
+    }
+
+    .ctn-preloader .animation-preloader {
+      z-index: 1000;
+    }
+
+    .ctn-preloader .animation-preloader .spinner {
+      -webkit-animation: spinner 1s infinite linear;
+      animation: spinner 1s infinite linear;
+      border-radius: 50%;
+      border: 3px solid rgba(0, 141, 201, 0.2);
+      border-top-color: #008dc9;
+      height: 9em;
+      margin: 0 auto 3.5em auto;
+      width: 9em;
+    }
+
+    .ctn-preloader .animation-preloader .txt-loading {
+      font: bold 5em 'Poppins', sans-serif;
+      text-align: center;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+    }
+
+    .ctn-preloader .animation-preloader .txt-loading .letters-loading {
+      color: rgba(0, 141, 201, 0.3);
+      position: relative;
+    }
+
+    .ctn-preloader .animation-preloader .txt-loading .letters-loading:before {
+      -webkit-animation: letters-loading 4s infinite;
+      animation: letters-loading 4s infinite;
+      color: #008dc9;
+      content: attr(data-text-preloader);
+      left: 0;
+      opacity: 0;
+      font-family: 'Poppins', sans-serif;
+      position: absolute;
+      top: -3px;
+      -webkit-transform: rotateY(-90deg);
+      transform: rotateY(-90deg);
+    }
+
+    .ctn-preloader .animation-preloader .txt-loading .letters-loading:nth-child(2):before {
+      -webkit-animation-delay: 0.2s;
+      animation-delay: 0.2s;
+    }
+
+    .ctn-preloader .animation-preloader .txt-loading .letters-loading:nth-child(3):before {
+      -webkit-animation-delay: 0.4s;
+      animation-delay: 0.4s;
+    }
+
+    .ctn-preloader .animation-preloader .txt-loading .letters-loading:nth-child(4):before {
+      -webkit-animation-delay: 0.6s;
+      animation-delay: 0.6s;
+    }
+
+    .ctn-preloader .animation-preloader .txt-loading .letters-loading:nth-child(5):before {
+      -webkit-animation-delay: 0.8s;
+      animation-delay: 0.8s;
+    }
+
+    .ctn-preloader .animation-preloader .txt-loading .letters-loading:nth-child(6):before {
+      -webkit-animation-delay: 1s;
+      animation-delay: 1s;
+    }
+
+    .ctn-preloader .animation-preloader .txt-loading .letters-loading:nth-child(7):before {
+      -webkit-animation-delay: 1.2s;
+      animation-delay: 1.2s;
+    }
+
+    .ctn-preloader .animation-preloader .txt-loading .letters-loading:nth-child(8):before {
+      -webkit-animation-delay: 1.4s;
+      animation-delay: 1.4s;
+    }
+
+    .ctn-preloader.dark .animation-preloader .spinner {
+      border-color: rgba(255, 255, 255, 0.2);
+      border-top-color: #fff;
+    }
+
+    .ctn-preloader.dark .animation-preloader .txt-loading .letters-loading {
+      color: rgba(255, 255, 255, 0.2);
+    }
+
+    .ctn-preloader.dark .animation-preloader .txt-loading .letters-loading:before {
+      color: #fff;
+    }
+
+    .ctn-preloader p {
+      font-size: 14px;
+      font-weight: 500;
+      text-transform: uppercase;
+      letter-spacing: 8px;
+      color: #3b3b3b;
+    }
+
+    .ctn-preloader .loader {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      font-size: 0;
+      z-index: 1;
+      pointer-events: none;
+    }
+
+    .ctn-preloader .loader .row {
+      height: 100%;
+    }
+
+    .ctn-preloader .loader .loader-section {
+      padding: 0px;
+    }
+
+    .ctn-preloader .loader .loader-section .bg {
+      background-color: #ffffff;
+      height: 100%;
+      width: 100%;
+      opacity: 1;
+      -webkit-transition: opacity 700ms cubic-bezier(0.77, 0, 0.175, 1);
+      transition: opacity 700ms cubic-bezier(0.77, 0, 0.175, 1);
+    }
+
+    .ctn-preloader .loader.dark_bg .loader-section .bg {
+      background: #111339;
+    }
+
+    @-webkit-keyframes spinner {
+      to {
+        -webkit-transform: rotateZ(360deg);
+        transform: rotateZ(360deg);
+      }
+    }
+
+    @keyframes spinner {
+      to {
+        -webkit-transform: rotateZ(360deg);
+        transform: rotateZ(360deg);
+      }
+    }
+
+    @-webkit-keyframes letters-loading {
+
+      0%,
+      75%,
+      100% {
+        opacity: 0;
+        -webkit-transform: rotateY(-90deg);
+        transform: rotateY(-90deg);
+      }
+
+      25%,
+      50% {
+        opacity: 1;
+        -webkit-transform: rotateY(0deg);
+        transform: rotateY(0deg);
+      }
+    }
+
+    @keyframes letters-loading {
+
+      0%,
+      75%,
+      100% {
+        opacity: 0;
+        -webkit-transform: rotateY(-90deg);
+        transform: rotateY(-90deg);
+      }
+
+      25%,
+      50% {
+        opacity: 1;
+        -webkit-transform: rotateY(0deg);
+        transform: rotateY(0deg);
+      }
+    }
+  </style>
 </head>
 
 <body class="no-scroll-y">
