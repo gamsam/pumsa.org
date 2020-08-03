0c0202dfad7d6030cd58b0f5773211bb4c7f0019
gamsam <paulshimbal@gmail.com>
2020-08-03 13:22:20 +0100

    tweaks

--- videos.php
+++ videos.php
@@ -41,12 +41,7 @@ include(ROOT_PATH . '/app/includes/header.php');
 <!--/ Intro Single End /-->
 
 <div class="col-md-10 offset-md-1">
-  <!-- <iframe width="100%" height="600" src="https://www.youtube.com/embed/Gu4u74YErb4?rel=0&amp;controls=1&amp&amp;showinfo=0&amp;modestbranding=1" frameborder="0"></iframe> -->
-  <iframe width="100%" src="https://youtu.be/nE0V515j1Os" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>
-
-<div class="col-md-10 offset-md-1">
-  <iframe width="100%" src="https://www.youtube.com/embed/Gu4u74YErb4?rel=0&amp;controls=1&amp&amp;showinfo=0&amp;modestbranding=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <iframe width="100%" src="https://youtu.be/mdJb-pMQ8rY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 
 
